94dd10c8bcfeaf39099e2b17eb60c9e2e0ff0613
Historia Montague <historia.montague@outlook.com>
2023-05-05 10:12:12 -0600

    Finished Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {useEffect, useState} from 'react'
+    
+const App = () => {
+    const [img, setImg] = useState(null)
+
+    useEffect(() => {
+        async function fetchData () {
+            const data = await fetch("https://dog.ceo/api/breeds/image/random")
+            const resp = await data.json()
+            setImg(resp.message
+        )}
+        fetchData()
+    }, [])
+    if(!img) {
+        return <p>Loading...</p>
+    }
+  return (
+    <img src={img} alt="A Random Dog"></img>
+  )
+}
+
+export default App
